73c251c130b92681597bb3967e9989c0e54b0eb1
Deepak Kumar <deepakplay14@gmail.com>
2023-12-03 13:33:54 +0530

    Vite react setup

---
+++ resources/js/App.jsx
@@ -0,0 +1,23 @@
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+// import Pusher from 'pusher-js';
+// import Echo from 'laravel-echo';
+import "@fontsource/roboto";
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+import "./App.scss";
+
+
+let root = ReactDOM.createRoot(document.getElementById('app'));
+
+root.render(
+    <React.StrictMode>
+        <h1>
+            Hello, React
+        </h1>
+    </React.StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/App.jsx',
             refresh: true,
         }),
+        react(),
+        svgr(),
+        eslint()
     ],
+    build: {
+        manifest: 'manifest.json',
+        commonjsOptions: {
+            transformMixedEsModules: true
+        }
+    }
 });
