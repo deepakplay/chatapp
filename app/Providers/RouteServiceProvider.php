d016a5d5a098621470e2182d837e91ac5b056c0d
Deepak Kumar <deepakplay14@gmail.com>
2023-12-04 22:24:24 +0530

    Route perfix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,13 +24,13 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        RateLimiter::for('api', function (Request $request) {
+        RateLimiter::for('v1', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
+                ->prefix('v1')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
